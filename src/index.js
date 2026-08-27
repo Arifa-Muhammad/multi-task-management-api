e5d0632841ce00ea625a1ea0dd06c5c8b72a8f40
Arifa-Muhammad <arifamuhammad943@gmail.com>
2026-08-27 18:14:20 +0500

    feat: add cookie-parser and cors middleware for enhanced request handling

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 //require('dotenv').config({path: './.enc'})
 import dotenv from "dotenv"// why we import dotenv bcz when main file excute, all .env early as possible, and used with experimental features
 import connectDB from "./db/dbConnection.js";
+import express from 'express'
 
 dotenv.config({
     path: './.env'
 })
 
+const app=express()
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error", (err)=>{
+        console.log("ERROR: ",err);
+        throw err;
 
-
+    })
+    app.listen(process.env.PORT || 5000, ()=>{
+        console.log(`server is running at port : ${process.env.PORT}`);
+        
+    })
+    
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed !!!", err);
+    
+})
 
 
 
@@ -31,9 +47,13 @@ connectDB();
 
 
 // import express from "express"
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants.js"
 
+// import dns from "dns";
+// dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-// const app= express()
+// const app= express();
 // (async ()=>{
 //     try {
 //         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
@@ -45,6 +65,8 @@ connectDB();
 //             console.log(`App is listening on port ${process.env.PORT}`);
     
 //         })
+//         console.log("MONGODB connected!! ");
+        
 //     } catch (error) {
 //         console.log("Error: ", error)
 //         throw error
